08896454ac023695abd4811e8eeae4dc5dae0998
Guido Queiroz <Takzzg@hotmail.com>
2021-07-28 21:40:19 -0300

    react router test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 function App() {
     return (
-        <div className="App">
-            <h1>Holis</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Link to="/">Home</Link>
+                <Link to="/carritos">Carritos</Link>
+                <Link to="/contacto">Contacto</Link>
+
+                <Switch>
+                    <Route exact={true} path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/carritos">
+                        <Carritos />
+                    </Route>
+                    <Route path="/contacto">
+                        <Contacto />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
+}
+
+const Home = () => {
+    return <h1>Home</h1>
+}
+const Carritos = () => {
+    return <h1>Carritos</h1>
+}
+const Contacto = () => {
+    return <h1>Contacto</h1>
 }
 
 export default App
