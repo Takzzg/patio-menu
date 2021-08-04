89c44e362a87b57d4fa1fe7b738c545282b7f16a
Guido Queiroz <Takzzg@hotmail.com>
2021-08-04 19:45:42 -0300

    map basic style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Home.scss'
 
 function Home() {
     return (
-        <div>
+        <div className="home">
+            <div className="mapTitle">¿ Donde Encontrarnos ?</div>
             <iframe
                 title="map"
-                width="600"
-                height="450"
+                className="map"
                 loading="lazy"
                 allowFullScreen
                 src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJP-14xnjn0pURqmo5f1cbcA4&key=${process.env.REACT_APP_GMAPS_API_KEY}`}
