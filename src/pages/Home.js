f816508ff462fea417fcfaff066f37f8b7d2c213
Guido Queiroz <Takzzg@hotmail.com>
2021-08-04 19:08:07 -0300

    embeded google maps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <iframe
+                title="map"
+                width="600"
+                height="450"
+                loading="lazy"
+                allowFullScreen
+                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJP-14xnjn0pURqmo5f1cbcA4&key=AIzaSyC9RNkKqT9TEqFLvN5xsmV4Iy9zQ-HAL94"
+            />
+        </div>
+    )
 }
 
 export default Home
