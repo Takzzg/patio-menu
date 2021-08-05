a6039d6b96f7a62de733d01e4d5d0f0f47a3628e
Guido Queiroz <Takzzg@hotmail.com>
2021-08-04 22:12:49 -0300

    instagram feed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import InstagramFeed from 'react-ig-feed'
 import './Home.scss'
 
 function Home() {
@@ -11,6 +12,10 @@ function Home() {
                 loading="lazy"
                 allowFullScreen
                 src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJP-14xnjn0pURqmo5f1cbcA4&key=${process.env.REACT_APP_GMAPS_API_KEY}`}
+            />
+            <InstagramFeed
+                token={process.env.REACT_APP_INSTA_TOKEN}
+                counter="6"
             />
         </div>
     )
