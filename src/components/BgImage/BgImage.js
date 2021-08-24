d670b89addb8eba359a1768103e5f9bf025a3331
Guido Queiroz <Takzzg@hotmail.com>
2021-08-24 20:39:47 -0300

    dataCarritos.json updated + minor visual adjusts

--- src/components/BgImage/BgImage.js
+++ src/components/BgImage/BgImage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Overlay } from '../index'
 import './BgImage.sass'
+const stockPhoto = '../images/carrito1.jpg'
 
 function BgImage(props) {
     const { foto, children, overlay } = props
     return (
-        <div className="bgImage" style={{ backgroundImage: `url(${foto})` }}>
+        <div
+            className="bgImage"
+            style={{ backgroundImage: `url(${foto ? foto : stockPhoto})` }}
+        >
             {overlay ? <Overlay>{children}</Overlay> : children}
         </div>
     )
