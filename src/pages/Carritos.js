25dc4703cc7f37d320a41376984f14586c7029e3
Guido Queiroz <Takzzg@hotmail.com>
2021-08-24 21:00:49 -0300

    carritos alphabetically sorted

--- src/pages/Carritos.js
+++ src/pages/Carritos.js
@@ -4,25 +4,35 @@ import dataCarritos from '../dataCarritos'
 import { Carrito, Category } from '../components'
 import './Carritos.sass'
 
+const sortedCarritos = dataCarritos.sort((a, b) => {
+    return a.nombre.localeCompare(b.nombre)
+})
+const cerveceros = sortedCarritos
+    .filter((carr) => {
+        return carr.cerveceria
+    })
+    .map((carr) => (
+        <Link key={carr.id} to={`/carritos/${carr.nombre}`}>
+            <Carrito carr={carr} />
+        </Link>
+    ))
+const comida = sortedCarritos
+    .filter((carr) => {
+        return !carr.cerveceria
+    })
+    .map((carr) => (
+        <Link key={carr.id} to={`/carritos/${carr.nombre}`}>
+            <Carrito carr={carr} />
+        </Link>
+    ))
+
 function Carritos() {
     return (
         <div className="contenedor-carritos">
             <Category nombre="Cerveza" />
-            {dataCarritos.map((carr) =>
-                carr.cerveceria ? (
-                    <Link key={carr.id} to={`/carritos/${carr.nombre}`}>
-                        <Carrito carr={carr} />
-                    </Link>
-                ) : null
-            )}
+            {cerveceros}
             <Category nombre="Comidas" />
-            {dataCarritos.map((carr) =>
-                !carr.cerveceria ? (
-                    <Link key={carr.id} to={`/carritos/${carr.nombre}`}>
-                        <Carrito carr={carr} />
-                    </Link>
-                ) : null
-            )}
+            {comida}
         </div>
     )
 }
